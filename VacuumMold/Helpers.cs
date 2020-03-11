d0a42a6a724374a89aec3a1239b2f43ee536ae27
Frank A. Krueger <fak@praeclarum.org>
2020-03-11 12:04:05 -0700

    Create shapes

---
+++ VacuumMold/Helpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+
+namespace VacuumMold
+{
+    public static class Helpers
+    {
+        public static Vector2 Xy (float x, float y) => new Vector2 ((float)x, (float)y);
+        public static Vector2 Xy (nfloat x, nfloat y) => new Vector2 ((float)x, (float)y);
+        public static Vector2 Xy (double x, double y) => new Vector2 ((float)x, (float)y);
+
+        public static Vector3 Xyz (float x, float y, float z) => new Vector3 ((float)x, (float)y, z);
+        public static Vector3 Xyz (nfloat x, nfloat y, nfloat z) => new Vector3 ((float)x, (float)y, (float)z);
+        public static Vector3 Xyz (double x, double y, double z) => new Vector3 ((float)x, (float)y, (float)z);
+        public static Vector3 Xyz (Vector2 xy, float z) => new Vector3 (xy.X, xy.Y, z);
+    }
+}
---
+++ VacuumMold/Shape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+using CoreGraphics;
+
+using static VacuumMold.Helpers;
+
+namespace VacuumMold
+{
+    public abstract class Shape
+    {
+        public abstract Vector2[] SamplePerimeter (float tolerance);
+    }
+
+    public class Box : Shape
+    {
+        public CGRect Frame { get; set; }
+
+        public override Vector2[] SamplePerimeter (float tolerance)
+        {
+            return new[] {
+                Xy (Frame.X, Frame.Y),
+                Xy (Frame.X + Frame.Width, Frame.Y),
+                Xy (Frame.X + Frame.Width, Frame.Y + Frame.Height),
+                Xy (Frame.X, Frame.Y + Frame.Height),
+            };
+        }
+    }
+}
