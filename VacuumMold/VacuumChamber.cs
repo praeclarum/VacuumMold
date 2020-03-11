159e0f78f5bb668261df62a29014b387d026e67e
Frank A. Krueger <fak@praeclarum.org>
2020-03-11 11:51:45 -0700

    Create vacuum chamber

---
+++ VacuumMold/VacuumChamber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using AppKit;
+using CoreGraphics;
+using Foundation;
+using SceneKit;
+
+namespace VacuumMold
+{
+    [Register ("VacuumChamber")]
+    public class VacuumChamber : SCNView
+    {
+        public VacuumChamber (CGRect frame)
+            : base (frame) => Initialize ();
+        public VacuumChamber (NSCoder coder)
+            : base (coder) => Initialize ();
+        public VacuumChamber (IntPtr handle)
+            : base (handle) => Initialize ();
+        public VacuumChamber (NSObjectFlag t)
+            : base (t) => Initialize ();
+
+        void Initialize ()
+        {
+            BackgroundColor = NSColor.Black;
+        }
+    }
+}
