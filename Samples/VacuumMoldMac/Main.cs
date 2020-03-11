996c7508494c37d86818b0dfbd5c7b2990357265
Frank A. Krueger <fak@praeclarum.org>
2020-03-11 11:33:02 -0700

    Initial commit

---
+++ Samples/VacuumMoldMac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace VacuumMoldMac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
---
+++ VacuumMold/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace VacuumMold
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
